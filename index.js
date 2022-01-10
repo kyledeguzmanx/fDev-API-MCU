24965f85633515866e3be566c63f2f456f89aea4
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-10 01:01:04 -0800

    GSheetReader callbacks introduced

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const GSheetReader = require("g-sheets-api");
+const config = require("./config")
+GSheetReader(
+    options,
+    (results) => {
+        //results instructions
+    },
+    (error) => {
+        //error instructions
+    }
+);
