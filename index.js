3107fd4c7bf901a4e774902632ffa12477426984
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-10 19:41:42 -0800

    send json file to fulfill get request

--- index.js
+++ index.js
@@ -11,17 +11,31 @@ GSheetReader(
     },
     (results) => {
         //results instructions
-        //console.log(results);
+        console.log(results);
+        const PORT = 8000;
+        const axios = require("axios");
+        const cheerio = require("cheerio");
+        const app = require("express")();
+
+        
+         app.get("/", (req,res) => {
+             res.json(results[Math.floor(Math.random() * results.length)]);
+         })
+
+        app.listen(PORT, () => console.log(`server running on ${PORT}`)); //starts server and listens on port 8000
     },
     (error) => {
         //error instructions
     }
 );
 
-const PORT = 8000;
-const axios = require("axios");
-const cheerio = require("cheerio");
+// const PORT = 8000;
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+// const app = require("express")();
 
-const app = require("express")();
+// app.get("/", (req,res) => {
+//     res.json("Welcome to api");
+// })
 
-app.listen(PORT, () => console.log(`server running on ${PORT}`));
+// app.listen(PORT, () => console.log(`server running on ${PORT}`)); //starts server and listens on port 8000
